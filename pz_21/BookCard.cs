bcb6ddc3092f36f6fb9a23027785522b0bb2ad6c
Золин Никита <zolinnikita69@gmail.com>
2023-02-19 18:09:52 +0500

    Добавьте файлы проекта.

---
+++ pz_21/BookCard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class BookCard
+    {
+
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Condition { get; set; }
+
+
+
+        public BookCard(int id, string author, string title, string year, string condition)
+        {
+            ID = id;
+            Author = author;
+            Title = title;
+            Year = year;
+            Condition = condition;            
+        }
+        public BookCard()
+        {
+            ID = 0;
+            Author = "неизвестный";
+            Title = "пусто";
+            Year = "неизвестно";
+            Condition = "неизвестно";
+        }
+        public void GetBookInfo()
+        {
+            Console.WriteLine($"ID: {ID}\nАвтор: {Author}\nНазвание: {Title} \nГод: {Year} \nCостаяние книги: {Condition}");
+        }
+    }
+}
---
+++ pz_21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCard bookCard = new BookCard(); 
+            BookCard bookCard2 = new BookCard(111111, "Дмитрий Гласов", "Белоснежка", "2015", "Вышла");
+            BookCard bookCard3 = new BookCard
+            {
+                Author = "Олег Ямаи",
+                ID = 111112,
+                Title = "путешествие щенка",
+                Year = "2010",
+                Condition = "Вышла"
+            };
+            BookCard bookCard4 = new BookCard
+            {
+               
+            };
+            bookCard.GetBookInfo();
+            bookCard2.GetBookInfo();
+            bookCard3.GetBookInfo();
+            bookCard4.GetBookInfo();
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
